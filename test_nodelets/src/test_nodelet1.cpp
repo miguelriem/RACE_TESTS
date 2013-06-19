7cba37b75c40c81625f5c394157027a08241b739
Miguel Riem <mriem@ua.pt>
2013-06-19 17:39:17 +0100

    mike adding comments and cleaning the test_nodelets package code

--- test_nodelets/src/test_nodelet1.cpp
+++ test_nodelets/src/test_nodelet1.cpp
@@ -34,22 +34,23 @@ namespace test_nodelet1_ns
 			}
 
 
+			/**
+			 * @brief Is called when a point cloud is received
+			 *
+			 * @param input a ptr to the pc
+			 */
 			void callback(const sensor_msgs::PointCloud2::ConstPtr& input)
 			{
-				//ROS_INFO("Nodelet1: Received a msg");
-
-				//sensor_msgs::PointCloud2::Ptr output(new sensor_msgs::PointCloud2());
-				//pub.publish(output);
-
 				static int i=0;
+
+				//Compute the new value of key1
 				i++;
 				std::string ss = "value " + boost::lexical_cast<std::string>(i);
 				ROS_INFO("This is the new value for key1=%s",ss.c_str());
+
+				//Set the new value for key1
 				leveldb::Status status;
 				status = db->Put(leveldb::WriteOptions(), "key1", ss);
-
-				sensor_msgs::PointCloud2 output_noptr;
-				pub.publish(output_noptr);
 
 				//print everything on the db
 				ROS_INFO("from nodelet 1: full db keys");
@@ -60,7 +61,6 @@ namespace test_nodelet1_ns
 				assert(it->status().ok());  // Check for any errors found during the scan
 				delete it;
 
-
 			}
 
 
@@ -69,12 +69,13 @@ namespace test_nodelet1_ns
 
 				//DB stuff
 				options.create_if_missing = true;
-				leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+				leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db); // open the databaser
 				assert(status.ok());
+
 				if (!status.ok()) std::cout << status.ToString() << std::endl;
 				ROS_INFO("DB created. Status is %s address is %p\n Initial DB status is", status.ToString().c_str(), db);
 
-				//print everything on the db
+				//print everything on the db. before adding anything else
 				leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
 				for (it->SeekToFirst(); it->Valid(); it->Next()) {
 					ROS_INFO("%s:%s",it->key().ToString().c_str(), it->value().ToString().c_str());
@@ -82,111 +83,25 @@ namespace test_nodelet1_ns
 				assert(it->status().ok());  // Check for any errors found during the scan
 				delete it;
 
-				//Add some stuff
+				//Add some stuff to the db
 				status = db->Put(leveldb::WriteOptions(), "key1", "value1"); //Put some info on the database
 
-				//std::string ptr_address = convertPointerToStringAddress(db);
-				//ROS_INFO("Passed address to string: %s", ptr_address.c_str());
 
-				//leveldb::DB* ptr_db;
-				//convertAddressStringToPointer(ptr_address, &ptr_db);
-
-				//ROS_INFO("DB pointer created. address is %p",ptr_db);
-
-
-				//ROS STUFF, publishers, subscribers and services
-				//ros::NodeHandle& private_nh = getPrivateNodeHandle();
-				ros::NodeHandle& private_nh = getNodeHandle();
-
-				pub = private_nh.advertise<sensor_msgs::PointCloud2>("/tn1_pc_out", 1);
+				// do some ros speficif initial configuration
+				ros::NodeHandle& private_nh = getNodeHandle(); //get a pointer to the node handle
+				pub = private_nh.advertise<sensor_msgs::PointCloud2>("/tn1_pc_out", 1); //advertive pc_out
 				sub = private_nh.subscribe("/camera/depth_registered/points", 1, &test_nodelet1::callback, this);
 				service = private_nh.advertiseService("/get_db_address", &test_nodelet1::get_db_address, this);
 
+				//Nodelet is prepared to run:
 				ROS_INFO("Starting nodelet1");
 
-				/* _________________________________
-				   |                                 |
-				   |     USING ANEESH functions      |
-				   |_________________________________| */
-
-
-				//ROS_INFO("STEP 1: allocate space");
-				//char* s;
-				//ROS_INFO("STEP 2: convert to char*");
-				//s = convert_leveldb_ptr2str(&db);
-				////Aneesh :- donit forget to remove the string later on from
-				////memory :P
-				//ROS_INFO("STEP 3: put in a std::string");
-				//std::string str_address = s;
-				//ROS_INFO("Address in str =%s", str_address.c_str());
-				////ROS_INFO("Address in str = %s", str_address.c_str());
-
-				//leveldb::DB* ptr_db=NULL;
-
-				//ROS_INFO("before DB pointer adress %p",ptr_db);
-				//ROS_INFO("STEP 4: Get back the ptr");
-				//convert_str2leveldb_ptr(s, &ptr_db);
-
-				//ROS_INFO("after DB pointer adress %p",ptr_db);
-
-				//ROS_INFO("STEP 5: try to put something in the database using the pointer");
-
-
-				//status = ptr_db->Put(leveldb::WriteOptions(), "key2", "value1");
-
-				//int x;   
-				//std::stringstream ss;
-				//ss << std::hex << "fffefffe";
-				//ss >> x;
-				//try
-				//{
-				//int x = boost::lexical_cast<int>(str_address.c_str());
-				//}
-				//catch( boost::bad_lexical_cast const& ) {
-				//std::cout << "Error: input string was not valid" << std::endl;
-				//}
-				//int int_address;
-				//s >> int_address;
-
-				//leveldb::DB* ptr_db = reinterpret_cast<leveldb::DB*>(str_address.c_str());
 			}
 
-			char *convert_leveldb_ptr2str(leveldb::DB **p)
-			{
-
-				ROS_INFO("1");
-				//ROS_INFO("inside DB pointer adress %p",p);
-				//printf("inside DB pointer adress %p\n",p);
-				fflush(stdout);
-				//int size_of_leveldb_ptr = sizeof(leveldb::DB *) +1;
-
-				//Aneesh: The reason I add 3 is because it seems the sizeof(pointer)
-				//does not include "0x" at the beginning of the printed pointer
-				//value. So we need to accommodate these two extra characters 
-				//+ 1 more for the empty space afterwards
-				int size_of_leveldb_ptr = sizeof(*p) +3;
-				printf("size is %d\n", size_of_leveldb_ptr);
-				printf("pointer val =  %p\n", *p);
-				ROS_INFO("2");
-				char *str_p = (char *)malloc(size_of_leveldb_ptr);
-				//char *str_p = (char *)malloc(8);
-				ROS_INFO("3");
-				sprintf(str_p, "%p", *p); 
-				ROS_INFO("4");
-
-				return str_p;
-			}
-
-			void convert_str2leveldb_ptr(char *str_p, leveldb::DB **ptr)
-			{
-				ROS_INFO("1");
-				leveldb::DB *new_ptr;
-				ROS_INFO("2");
-				sscanf(str_p,"%p", &new_ptr);
-				ROS_INFO("3");
-				*ptr = new_ptr;
-				ROS_INFO("4");
-			}
+/* _________________________________
+  |                                 |
+  |           VARIABLES             |
+  |_________________________________| */
 
 			ros::Publisher pub;
 			ros::Subscriber sub;
--- test_nodelets/src/test_nodelet2.cpp
+++ test_nodelets/src/test_nodelet2.cpp
@@ -19,43 +19,49 @@ namespace test_nodelet2_ns
 		private:
 			virtual void onInit() //mandatory method for nodelet manager to call
 			{
-				int* crazy_ptr = (int *)0x604769;
-
+				//Declare a node handle
 				ros::NodeHandle& private_nh = getPrivateNodeHandle();
-				//private_nh.getParam("value", value_);
-				pub = private_nh.advertise<sensor_msgs::PointCloud2>("/tn2_pc_out", 1);
+
+				//Initial config of publish subscribers
+				//pub = private_nh.advertise<sensor_msgs::PointCloud2>("/tn2_pc_out", 1);
 				//sub = private_nh.subscribe("/tn1_pc_out", 1, &test_nodelet2::callback, this);
 				sub = private_nh.subscribe("/camera/depth_registered/points", 1, &test_nodelet2::callback, this);
 
+				//Config service clients
 				ros::ServiceClient client = private_nh.serviceClient<test_nodelets::get_db_address>("/get_db_address");
+
+				//get db address by calling /get_db_address service
 				test_nodelets::get_db_address srv;
 				if (client.call(srv))
 				{
 					convertAddressStringToPointer(srv.response.mem_address, &ptr_db);
-					ROS_INFO("Address to db is %s. Creating local pointer to db (pointed to address %p)", srv.response.mem_address.c_str(),ptr_db );
+					ROS_INFO("Address to db is %s. Creating local pointer to db (pointed to address %p)", srv.response.mem_address.c_str(),ptr_db);
 				}
 				else
 				{
 					ROS_ERROR("Nodelet2: Failed to call service get_db_address");
 				}
 
-				
 			}
 
 			void callback(const sensor_msgs::PointCloud2::ConstPtr& input)
 			{
-				sensor_msgs::PointCloud2::Ptr output(new sensor_msgs::PointCloud2());
-				ROS_INFO("Nodelet2: Received a msg");
-				//NODELET_DEBUG("Adding %f to get %f", value_, output->data);
-				pub.publish(output);
-
 				static int i=77;
+				//sensor_msgs::PointCloud2::Ptr output(new sensor_msgs::PointCloud2());
+				//ROS_INFO("Nodelet2: Received a msg");
+				////NODELET_DEBUG("Adding %f to get %f", value_, output->data);
+				//pub.publish(output);
+
+				//Define a new value for key 3	
 				i++;
 				std::string ss = "value " + boost::lexical_cast<std::string>(i);
-
 				ROS_INFO("Adding (from nodelet2) another key 3 = %s", ss.c_str());
+
+				//putting new key 3 value on the db
 				leveldb::Status status;
 				status = ptr_db->Put(leveldb::WriteOptions(), "key3", ss); //Put some info on the database
+
+				//putting value on key 1
 				status = ptr_db->Put(leveldb::WriteOptions(), "key1", ss); //Put some info on the database
 
 			}
